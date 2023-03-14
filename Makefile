fe62021e85f6e0895f9f888055113f08de2a82d3
Philipp Lugovoy <i376765616@yandex.ru>
2023-03-14 14:06:59 +0500

    Step 2

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 install:
 	npm install
 
+lint:
+	npx eslint .
+
 publish:
 	npm publish --dry-run
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
+import { program } from 'commander';
 
 program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .parse()
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .parse();
